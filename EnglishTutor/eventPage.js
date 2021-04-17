ab986fcb8d63dd384d7f0ad8f692be23b141409b
Alp Niksarlı <alp.niksarli@gmail.com>
2021-04-17 23:37:46 +0300

    duplicated words not allowed

--- EnglishTutor/eventPage.js
+++ EnglishTutor/eventPage.js
@@ -9,62 +9,46 @@ chrome.contextMenus.create(contextMenuItem);
 console.log("context menu created");
 
 // initializing words list
-var wordsData = [];
+var words = [];
 
 // adding an event listener for right click
 chrome.contextMenus.onClicked.addListener(function(clickedData) {
     if (clickedData.menuItemId == "getWord" && clickedData.selectionText) {
-        let word = getSearchableWord(clickedData.selectionText);
-        let wordData = getData(word);
-        console.log(wordData);
 
-        if (wordData) {
-            wordsData.push(wordData);
-        } else {
-            chrome.storage.sync.set({"wordsData": "hello world"}, () => {console.log("wordData is empty")})
+        var inList = false;
+        let word = getSearchableWord(clickedData.selectionText)
+        let date = new Date().toLocaleDateString();
+        function checkAlreadyInList(el) {
+            return el.word !== word;
         }
 
-        chrome.storage.sync.set({"wordsData": wordsData}, function() {
+        for (const wordObj of words) {
+            if (word == wordObj.word) {
+                var inList = true;
+            }
+        }
+
+        if (word && !inList) {
+            words.push({
+                word: word,
+                date: date
+            });
+        } 
+
+        chrome.storage.sync.set({"wordsData": words}, function() {
             let notifOptions = {
                 type: "basic",
                 iconUrl: "../icons/logo.png",
                 title: "More Than 10 Words",
                 message: `By searching for ${clickedData.selectionText} you have learned more than 10 words`,
             }
-            if (wordsData.length >= 10) {
+            if (words.length >= 10) {
                 chrome.notifications.create("textNotif",notifOptions);
             }
         })
     }
 })
 
-function getData(searchable) {
-    var queryUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchable}`
-
-    const wordData = fetch(queryUrl)
-    .then(res => {
-        if (res.ok) {
-            return res.json()
-        } else {
-            return false;
-        }
-    })
-
-    .catch(error => console.log(error))
-    
-    wordData.then(res => {
-        if (!res) {
-            return false;
-        }
-        let data = {
-            date: new Date().toLocaleDateString(),
-            word: res[0].word,
-            form: res[0].meanings[0].partOfSpeech,
-            definition: res[0].meanings[0].definitions[0].definition
-        }
-        return data;
-    })
-}
 
 function getSearchableWord(text) {
     let searchable = text.trim();
--- EnglishTutor/js/script.js
+++ EnglishTutor/js/script.js
@@ -1,4 +1,4 @@
-const boxes = document.querySelector(".boxes");
+var boxes = document.querySelector(".boxes");
 
 // array remove function
 Array.prototype.remove = function() {
@@ -12,6 +12,9 @@ Array.prototype.remove = function() {
     return this;
 };
 
+// if you want to restart the list
+// chrome.storage.sync.set({"wordsData": []});
+
 
 chrome.storage.sync.get(["wordsData"], function(result) {
     if (!result.wordsData) {
@@ -19,13 +22,26 @@ chrome.storage.sync.get(["wordsData"], function(result) {
         return;
     }
 
-    console.log(result.wordsData);
+    result.wordsData.forEach(wordInfo => {
 
-    result.wordsData.forEach(wordData => {
-        console.log(wordData);
-        let newBox = getNewBox(wordData);
+        let wordData = getData(wordInfo.word);
+        wordData.then(res => {
+            if (!res) {
+                return false;
+            }
+    
+            let data = {
+                date: wordInfo.date,
+                word: res[0].word,
+                form: res[0].meanings[0].partOfSpeech,
+                definition: res[0].meanings[0].definitions[0].definition
+            }
 
-        boxes.appendChild(newBox);
+            if (data) {
+                let newBox = getNewBox(data);
+                boxes.appendChild(newBox);
+            }
+            })
     });
 })
 
@@ -35,6 +51,7 @@ function getNewBox(data) {
 
     let newBox = document.createElement("div");
     newBox.classList.add("box");
+    let dictUrl = `https://dictionary.cambridge.org/dictionary/english/${data.word}`
 
     newBox.innerHTML = `
         <div class="date subtitle">${data.date}</div>
@@ -42,7 +59,57 @@ function getNewBox(data) {
             <div class="word">${data.word}</div>
             <div class="form subtitle">${data.form}</div>
         </div>
-        <div class="def">${data.definition}</div>`
+        <div class="def">${data.definition}</div>
+        <div class="buttons">
+        <a href="${dictUrl}" class="btn more-info">More Info</a>
+        <button data-word="${data.word}" class="btn delete">Delete</button>
+        </div>`
 
     return newBox;
 }
+
+function getData(searchable) {
+    var queryUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchable}`
+
+    const wordData = fetch(queryUrl)
+    .then(res => {
+        if (res.ok) {
+            return res.json()
+        } else {
+            return false;
+        }
+    })
+
+    .catch(error => console.log(error))
+
+    return wordData;
+    
+}
+
+// events 
+
+
+setTimeout(() => {
+    var deleteBtns = document.querySelectorAll("button.delete");
+
+    console.log(deleteBtns);
+    for (const btn of deleteBtns) {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            e.target.parentElement.parentElement.style.display = "none";
+            chrome.storage.sync.get("wordsData", function(wordsData) {
+                let words = wordsData.wordsData;
+
+                for (let i = 0; i < deleteBtns.length; i++) {
+                    if (e.target = deleteBtns[i]) {
+                        var index = i;
+                    }
+                }
+                words = words.splice(index,1);
+                chrome.storage.sync.set({"wordsData": words})
+            })    
+        });
+    }
+},1600)
+
